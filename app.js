1f93f4b473e139f1332bdc9f66bce5e7840b140a
Joey Holland <joeyholland15@Joeys-MacBook-Pro.local>
2016-03-07 13:21:35 -0800

    Adds logic to check for a winner or a tie, all cases seem to work, consider adding tests

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ angular.module('TicTacToe', [])
     //paths for x and o images
     $scope.xImg = "./x.jpg"
     $scope.oImg = "./o.jpg"
-    //keeps track of which squares have been clicked
+    
+    //object that tracks each of the nine squares. Properties include
+    //whether or not the square has been clicked, adjacent squares needed
+    //to check whether or not a click won the game, and which player (x or o)
+    //clicked on it. 
     $scope.squares = {
       one: {
         clicked: false, 
@@ -59,20 +63,45 @@ angular.module('TicTacToe', [])
       },
     }
 
+    //keeps track of how many turns have been played
+    $scope.turns = 0; 
+
     //first player will start as x
     $scope.current = "x"
 
     //boolean variable to keep track of whether or not the game is over
-    $scope.gameOver = false;
+    $scope.gameOver;
 
     //function to check to see if the game is over, whether there is a winner or a tie
     $scope.isOver = function(squareNumber) {
-      var squares = $scope.squares;
+      console.log($scope.turns);
+      var squares = $scope.squares
+      var square = squares[squareNumber];
+      var h = square.horizontal;
+      var v = square.vertical;
+      var d = square.diagonal; 
+      var winnerString = 'Congratulations! '+ square.player.toUpperCase() + ' Wins!';
+      var tieString = 'Tie!'
+      if($scope.turns === 9) {
+        $scope.gameOver = tieString;
+      }
+      if(square.player === squares[h[0]].player && square.player === squares[h[1]].player) {
+        $scope.gameOver = winnerString;
+      } else if (square.player === squares[v[0]].player && square.player === squares[v[1]].player) {
+        $scope.gameOver = winnerString;
+      } else if (squareNumber === 'five' && square.player === squares[d[0]].player && square.player === squares[d[1]].player) {
+        $scope.gameOver = winnerString;
+      } else if (squareNumber === 'five' && square.player === squares[d[2]].player && square.player === squares[d[3]].player) {
+        $scope.gameOver = winnerString;
+      } else if (square.player === squares[d[0]].player && square.player === squares[d[1]].player) {
+        $scope.gameOver = winnerString;
+      }
     }
 
     //function for ng-click on any square. Will toggle image and player status for each
     //sqaure on the board when clicked and update the current player as well. 
     $scope.toggleSquareImg = function (squareNumber) {
+      $scope.turns++;
       var square = $scope.squares[squareNumber] 
       if(square.clicked === false) {
         var img = $scope.current === "x" ? $scope.xImg : $scope.oImg; 
