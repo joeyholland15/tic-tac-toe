7802373a9a17b59a3c9816cd77b83b4dd450a4de
Joey Holland <joeyholland15@Joeys-MacBook-Pro.local>
2016-03-07 10:54:18 -0800

    Updates app.js to provide reference to image paths and current player on scope

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+//angular module to set as ng-app
 angular.module('TicTacToe', [])
+  //controller for the board container
+  .controller('BoardController', function($scope) {
+    //paths for x and o images
+    $scope.xImg = "./x.jpg"
+    $scope.oImg = "./o.jpg"
+    //first player will start as x
+    $scope.current = "x"
+    //function for toggling the current player between x and o
+    $scope.toggleCurrent = function() {
+      $scope.current === "x" ? $scope.current = "o" : $scope.current = "x";
+    }
+  })
