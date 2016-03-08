75427d8b3eed1312665b407e85690f20be20c5c1
Joey Holland <joeyholland15@Joeys-MacBook-Pro.local>
2016-03-07 20:14:15 -0800

    Updates controller to display boards with more than 3 rows

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ angular.module('TicTacToe', [])
     $scope.xImg = "./x.jpg"
     $scope.oImg = "./o.jpg"
 
-    $scope.image = "./x.jpg";
+    //boolean variable to keep track of whether or not the game is over
+    $scope.gameOver;
+
+    $scope.turns = 0; 
+
+    $scope.current = "x"
 
     //the number of rows on the board (defaults to 3)
-    $scope.rows = 5; 
+    $scope.rows = 3; 
 
     //initialize board matrix
-    $scope.board = [];
+    $scope.board = []; 
 
     $scope.createBoard = function() {
       //initialize count, which acts as a running total for all cells
@@ -45,113 +50,26 @@ angular.module('TicTacToe', [])
 
     //initial createBoard call to create and display board
     $scope.createBoard();
-    
-    console.log($scope.board);
 
-    // object that tracks each of the nine squares. Properties include
-    // whether or not the square has been clicked, adjacent squares needed
-    // to check whether or not a click won the game, and which player (x or o)
-    // clicked on it. 
-    // $scope.squares = {
-    //   one: {
-    //     clicked: false, 
-    //     vertical: ['two','four'], 
-    //     horizontal: ['two','three'], 
-    //     diagonal: ['five', 'nine']
-    //   },
-    //   two: {
-    //     clicked: false, 
-    //     vertical: ['five','eight'], 
-    //     horizontal: ['one','three'], 
-    //   },
-    //   three: {
-    //     clicked: false, 
-    //     vertical: ['six','nine'], 
-    //     horizontal: ['one','two'], 
-    //     diagonal: ['five', 'seven']
-    //   },
-    //   four: {
-    //     clicked: false, 
-    //     vertical: ['one','seven'], 
-    //     horizontal: ['five','six'], 
-    //   },
-    //   five: {
-    //     clicked: false,
-    //     vertical: ['two','eight'], 
-    //     horizontal: ['four','six'], 
-    //     diagonal: ['one', 'nine', 'three', 'seven']
-    //   },
-    //   six: {
-    //     clicked: false, 
-    //     vertical: ['three','nine'], 
-    //     horizontal: ['four','five'], 
-    //   },
-    //   seven: {
-    //     clicked: false, 
-    //     vertical: ['four','one'], 
-    //     horizontal: ['eight','nine'], 
-    //     diagonal: ['five', 'three']
-    //   },
-    //   eight: {
-    //     clicked: false, 
-    //     vertical: ['two','five'], 
-    //     horizontal: ['seven','nine'], 
-    //   },
-    //   nine: {
-    //     clicked: false, 
-    //     vertical: ['three','six'], 
-    //     horizontal: ['seven','eight'], 
-    //     diagonal: ['five', 'one']
-    //   },
+    // $scope.calcWinner = function(row, col) {
+
     // }
-
-    //keeps track of how many turns have been played
-    $scope.turns = 0; 
-
-    //first player will start as x
-    $scope.current = "x"
-
-    //boolean variable to keep track of whether or not the game is over
-    $scope.gameOver;
-
-    //function to check to see if the game is over, whether there is a winner or a tie
-    $scope.isOver = function(squareNumber) {
-      console.log($scope.turns);
-      var squares = $scope.squares
-      var square = squares[squareNumber];
-      var h = square.horizontal;
-      var v = square.vertical;
-      var d = square.diagonal; 
-      var winnerString = 'Congratulations! '+ square.player.toUpperCase() + ' Wins!';
-      var tieString = 'Tie!'
-      if($scope.turns === 9) {
-        $scope.gameOver = tieString;
-      }
-      if(square.player === squares[h[0]].player && square.player === squares[h[1]].player) {
-        $scope.gameOver = winnerString;
-      } else if (square.player === squares[v[0]].player && square.player === squares[v[1]].player) {
-        $scope.gameOver = winnerString;
-      } else if (squareNumber === 'five' && square.player === squares[d[0]].player && square.player === squares[d[1]].player) {
-        $scope.gameOver = winnerString;
-      } else if (squareNumber === 'five' && square.player === squares[d[2]].player && square.player === squares[d[3]].player) {
-        $scope.gameOver = winnerString;
-      } else if (square.player === squares[d[0]].player && square.player === squares[d[1]].player) {
-        $scope.gameOver = winnerString;
-      }
-    }
-
-    //function for ng-click on any square. Will toggle image and player status for each
-    //sqaure on the board when clicked and update the current player as well. 
-    $scope.toggleSquareImg = function (squareNumber) {
-      $scope.turns++;
-      var square = $scope.squares[squareNumber] 
-      if(square.clicked === false) {
-        var img = $scope.current === "x" ? $scope.xImg : $scope.oImg; 
-        square.clicked = true;
-        square.player = $scope.current
-        square.img = img; 
+    
+    $scope.toggleCell = function(row, col) {
+      //if the selected cell has not already been selected
+      if(!$scope.board[row][col].clicked) {
+        //set $scope image property to contain the appropriate image path
+        $scope.image = $scope.current === "x" ? $scope.xImg : $scope.oImg;
+        //sets the clicked property of the appropriate cell to true to ng-show image 
+        $scope.board[row][col].clicked = true;
+        //sets the image according to current player on that cell
+        $scope.board[row][col].image = $scope.image;
+        //toggles current player between x and o
         $scope.current === "x" ? $scope.current = "o" : $scope.current = "x";
-        $scope.isOver(squareNumber)
+        //increment turns played
+        $scope.turns++;
+        //run function to see if this play has ended the game
+        //$scope.calcWinner(row, col)
       }
-    }
+    } 
   })
